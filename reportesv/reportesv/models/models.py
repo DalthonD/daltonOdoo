2fafa100a26b7518af6a5258fac3cd6cd2d395df
Dalton Díaz <carlos.diaz@strategi-k.com>
2019-04-12 12:16:16 -0600

    asignación de default para mes

--- reportesv/reportesv/models/models.py
+++ reportesv/reportesv/models/models.py
@@ -476,7 +476,7 @@ class sv_consumer_sales_reportWizard(models.TransientModel):
     _description = "Wizard para reporte de consumidores"
 
     company_id=fields.Many2one('res.company', string="Company", help='Company',default=lambda self: self.env.user.company_id.id)
-    date_month = fields.Selection([('1','Enero'),('2','Febrero'),('3','Marzo'),('4','Abril'),('5','Mayo'),('6','Junio'),('7','Julio'),('8','Agosto'),('9','Septiembre'),('10','Octubre'),('11','Noviembre'),('12','Diciembre')],string='Mes de facturación',default=1,required=True)
+    date_month = fields.Selection([('1','Enero'),('2','Febrero'),('3','Marzo'),('4','Abril'),('5','Mayo'),('6','Junio'),('7','Julio'),('8','Agosto'),('9','Septiembre'),('10','Octubre'),('11','Noviembre'),('12','Diciembre')],string='Mes de facturación', default='1',required=True)
     date_year = fields.Integer("Año de facturación", default=2018 ,requiered=True)
 
     @api.multi
