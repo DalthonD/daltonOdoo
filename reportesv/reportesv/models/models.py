f838cbafa02049a83652cba4174b2fb47a1466e5
Dalton Díaz <carlos.diaz@strategi-k.com>
2019-04-12 12:03:59 -0600

    Modificación vista wizard

--- reportesv/reportesv/models/models.py
+++ reportesv/reportesv/models/models.py
@@ -476,8 +476,8 @@ class sv_consumer_sales_reportWizard(models.TransientModel):
     _description = "Wizard para reporte de consumidores"
 
     company_id=fields.Many2one('res.company', string="Company", help='Company',default=lambda self: self.env.user.company_id.id)
-    date_month = fields.Selection([('1','Enero'),('2','Febrero'),('3','Marzo'),('4','Abril'),('5','Mayo'),('6','Junio'),('7','Julio'),('8','Agosto'),('9','Septiembre'),('10','Octubre'),('11','Noviembre'),('12','Diciembre')],string='Mes de facturación',required=True)
-    date_year = fields.Integer("Año de facturación", requiered=True)
+    date_month = fields.Selection([('1','Enero'),('2','Febrero'),('3','Marzo'),('4','Abril'),('5','Mayo'),('6','Junio'),('7','Julio'),('8','Agosto'),('9','Septiembre'),('10','Octubre'),('11','Noviembre'),('12','Diciembre')],string='Mes de facturación',default=1,required=True)
+    date_year = fields.Integer("Año de facturación", default=2018 ,requiered=True)
 
     @api.multi
     def check_report(self):
