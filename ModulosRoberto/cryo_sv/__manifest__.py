fd3332e849ee49ddc93464ca87646a38bf7aacfe
Dalton Diaz <dm15012@ues.edu.sv>
2019-04-16 12:46:40 -0600

    Obtencion de todos los modulos necesarios

---
+++ ModulosRoberto/cryo_sv/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "CRYO SV",
+    "category": 'Sales',
+    "summary": """
+       Localizacion de CRYO .""",
+    "description": """
+	   Registra la orden de produccion para CRYO
+	
+    """,
+    "sequence": 1,
+    "author": "Strategi-k",
+    "website": "http://strategi-k.com",
+    "version": '12.0.0.4',
+    "depends": ['sale'],
+    "data": [
+        'security/ir.model.access.csv'
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+
+}
