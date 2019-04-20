a81c698f0e6c084cb15906ac0b8a981fd4aa4147
Dalton Diaz <carlos.diaz@strategi-k.com>
2019-04-20 17:19:44 -0600

    modificaciones

--- reportesv/reportesv/models/models.py
+++ reportesv/reportesv/models/models.py
@@ -364,127 +364,22 @@ class sv_consumer_sales_report(models.Model):
     retenido = fields.Float("Retenido")
     estado = fields.Char("Estado")
 
-    @api.model_cr
-    def init(self):
-        self.env.cr.execute("""CREATE OR REPLACE VIEW strategiksv_reportesv_sales_consumer_report AS (Select
-    10000000+SS.id as id
-    ,SS.Fecha
-	,SS.grupo
-	,min(SS.Factura) as DELNum
-	,max(SS.Factura) as ALNum
-	,sum(SS.exento) as Exento
-	,sum(SS.GravadoLocal) as GravadoLocal
-	,sum(SS.GravadoExportacion) as GravadoExportacion
-	,Sum(SS.ivaLocal) as IvaLocal
-	,Sum(SS.ivaexportacion) as IvaExportacion
-	,Sum(SS.retenido) as Retenido
-	,estado
-FROM (
-select 20000000+S.id as id
-    ,S.fecha
-	,S.factura
-	,S.estado
-	,S.grupo
-	,S.exento
-	,case
-		when S.sv_region='Local' then S.Gravado
-		else 0.00 end as GravadoLocal
-	,case
-		when S.sv_region!='Local' then S.Gravado
-		else 0.00 end as GravadoExportacion
-	,case
-		when S.sv_region='Local' then S.Iva
-		else 0.00 end as IvaLocal
-	,case
-		when S.sv_region!='Local' then S.Iva
-		else 0.00 end as IvaExportacion
-	,S.Retenido
-from(
-select 30000000+ai.id as id, ai.date_invoice as fecha
-	,coalesce(ai.reference,ai.number) as factura
-	,'valid' as estado
-	,FG.grupo
-	,afp.sv_region
-	,/*Calculando el gravado (todo lo que tiene un impuesto aplicado de iva)*/
-     (select coalesce(sum(price_subtotal_signed),0.00)
-      from account_invoice_line ail
-      where invoice_id=ai.id
-	      and exists(select ailt.tax_id
-					from account_invoice_line_tax ailt
-				        inner join account_tax atx on ailt.tax_id=atx.id
-				        inner join account_tax_group atg on atx.tax_group_id=atg.id
-			         where ailt.invoice_line_id=ail.id and atg.name='iva')
-      ) as Gravado,
-      /*Calculando el excento que no tiene iva*/
-     (Select coalesce(sum(price_subtotal_signed),0.00)
-      from account_invoice_line ail
-      where invoice_id=ai.id
-	      and not exists(select ailt.tax_id
-						 from account_invoice_line_tax ailt
-				             inner join account_tax atx on ailt.tax_id=atx.id
-				             inner join account_tax_group atg on atx.tax_group_id=atg.id
-			             where ailt.invoice_line_id=ail.id and atg.name='iva')
-      ) as Exento
-      ,/*Calculando el iva*/
-      (Select coalesce(sum(ait.amount),0.00)
-       from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
-       where invoice_id=ai.id
-	       and atg.name='iva'
-       ) as Iva
-	   ,/*Calculando el retenido*/
-      (Select coalesce(sum(ait.amount),0.00)
-       from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
-       where invoice_id=ai.id
-	       and atg.name='retencion'
-       ) as Retenido
-from account_invoice ai
-	inner join res_partner rp on ai.partner_id=rp.id
-	inner join (select * from FacturasAgrupadas( 1, 1 , 2018 , 5 )) FG on ai.id=FG.invoice_id
-	left join account_fiscal_position afp on ai.fiscal_position_id=afp.id
-where ai.type='out_invoice'
-	and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
-	and afp.sv_contribuyente=False
-	and ai.state in ('open','paid')
-
-union
-
-select 40000000+ai.id as id, ai.date_invoice as fecha
-	,coalesce(ai.reference,ai.number) as factura
-	,ai.state as estado
-	,FG.grupo
-	,afp.sv_region
-	,0.0 as Gravado
-	,0.0 as Exento
-    ,0.0 as Iva
-	,0.0 as Retenido
-from account_invoice ai
-	inner join res_partner rp on ai.partner_id=rp.id
-    inner join (select * from facturasAgrupadas( 1 , 1, 2018 , 5)) FG on ai.id=FG.invoice_id
-	left join account_fiscal_position afp on ai.fiscal_position_id=afp.id
-where ai.type='out_invoice'
-	and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
-	and afp.sv_contribuyente=False
-	and ai.state in ('cancel')
-)S )SS group by SS.fecha, SS.Grupo,SS.estado, SS.id order by SS.fecha, SS.Grupo)""")
-
 class sv_consumer_sales_reportWizard(models.TransientModel):
     _name = "report_wizard"
-    _description = "Wizard para reporte de consumidores"
+    _description = "Wizard para reportes"
 
     company_id=fields.Many2one('res.company', string="Company", help='Company',default=lambda self: self.env.user.company_id.id)
     date_month = fields.Selection([('1','Enero'),('2','Febrero'),('3','Marzo'),('4','Abril'),('5','Mayo'),('6','Junio'),('7','Julio'),('8','Agosto'),('9','Septiembre'),('10','Octubre'),('11','Noviembre'),('12','Diciembre')],string='Mes de facturación', default='1',required=True)
     date_year = fields.Integer("Año de facturación", default=2018 ,requiered=True)
+    show_serie = fields.Boolean("Ventas a Consumidor", default=False)
+    serie_lenght = fields.Integer("Agrupación de facturas", default = 1)
 
     @api.multi
     def check_report(self):
         data = {}
-        data['form'] = self.read(['company_id', 'date_month', 'date_year'])[0]
+        data['form'] = self.read(['company_id', 'date_month', 'date_year', 'serie_lenght', 'show_serie'])[0]
         return self._print_report(data)
 
     def _print_report(self, data):
-        data['form'].update(self.read(['company_id','date_month','date_year'])[0])
-        return self.env['strategiksv.reportesv.sales_consumer.report']
+        data['form'].update(self.read(['company_id','date_month','date_year','serie_lenght', 'show_serie'])[0])
+        #return self.env['strategiksv.reportesv.sales_consumer.report']
