148d447b1cb98a6c3ba0e4cd3fafd85c25d82579
Dalton Diaz <carlos.diaz@strategi-k.com>
2019-04-20 20:22:49 -0600

    modificacion de modelo

--- reportesv/reportesv/models/models.py
+++ reportesv/reportesv/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 from dateutil.parser import parse
 from odoo.exceptions import UserError
 
-class sv_purchase_report(models.Model):
+class sv_purchase_report(models.AbstractModel):
     _name = 'strategiksv.reportesv.purchase.report'
     _description = "Reporte de Compras"
     _auto = False
@@ -28,94 +28,89 @@ class sv_purchase_report(models.Model):
     retencion3 = fields.Float("Retención a Terceros")
 
     @api.model_cr
-    def init(self, company_id, date_year, date_month):
-        self.env.cr.execute("""CREATE OR REPLACE VIEW strategiksv_reportesv_purchase_report AS (select * from (
-select 10000000+ai.id as id
-    ,ai.company_id as company_id
-    ,date_part('year',COALESCE(ai.sv_fecha_tax,ai.date_invoice)) as anio
-    ,date_part('month',COALESCE(ai.sv_fecha_tax,ai.date_invoice)) as mes
-    ,ai.date_invoice as fecha
-	,ai.reference as factura
-	,rp.name as proveedor
-	,rp.vat as NRC
-	,case
-		when rp.country_id=211 then False
-		when rp.country_id is null then False
-		else True end as Importacion
-	,/*Calculando el gravado (todo lo que tiene un impuesto aplicado de iva)*/
+    @classmethod
+    def create_view(cls, company_id, date_year, date_month):
+        sql = """CREATE OR REPLACE VIEW strategiksv_reportesv_purchase_report AS (select * from (select ai.date_invoice as fecha
+    ,ai.reference as factura
+    ,rp.name as proveedor
+    ,rp.vat as NRC
+    ,case
+        when rp.country_id=211 then False
+        when rp.country_id is null then False
+        else True end as Importacion
+    ,/*Calculando el gravado (todo lo que tiene un impuesto aplicado de iva)*/
      (select coalesce(sum(price_subtotal_signed),0.00)
       from account_invoice_line ail
       where invoice_id=ai.id
-	      and exists(select ailt.tax_id
-					from account_invoice_line_tax ailt
-				        inner join account_tax atx on ailt.tax_id=atx.id
-				        inner join account_tax_group atg on atx.tax_group_id=atg.id
-			         where ailt.invoice_line_id=ail.id and atg.name='iva')
+          and exists(select ailt.tax_id
+                    from account_invoice_line_tax ailt
+                        inner join account_tax atx on ailt.tax_id=atx.id
+                        inner join account_tax_group atg on atx.tax_group_id=atg.id
+                     where ailt.invoice_line_id=ail.id and atg.name='iva')
       ) as Gravado,
       /*Calculando el excento que no tiene iva*/
      (Select coalesce(sum(price_subtotal_signed),0.00)
       from account_invoice_line ail
       where invoice_id=ai.id
-	      and not exists(select ailt.tax_id
-						 from account_invoice_line_tax ailt
-				             inner join account_tax atx on ailt.tax_id=atx.id
-				             inner join account_tax_group atg on atx.tax_group_id=atg.id
-			             where ailt.invoice_line_id=ail.id and atg.name='iva')
+          and not exists(select ailt.tax_id
+                         from account_invoice_line_tax ailt
+                             inner join account_tax atx on ailt.tax_id=atx.id
+                             inner join account_tax_group atg on atx.tax_group_id=atg.id
+                         where ailt.invoice_line_id=ail.id and atg.name='iva')
       ) as Exento
       ,/*Calculando el iva*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='iva'
+           and atg.name='iva'
        ) as Iva
-	   ,/*Calculando el retenido*/
+       ,/*Calculando el retenido*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='retencion'
+           and atg.name='retencion'
        ) as Retenido
-	    ,/*Calculando el percibido*/
+        ,/*Calculando el percibido*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='percepcion'
+           and atg.name='percepcion'
        ) as Percibido
          ,/*Calculando el excluido*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='excluido'
+           and atg.name='excluido'
        ) as Excluido
-	   ,/*Calculando el retencion a terceros*/
+       ,/*Calculando el retencion a terceros*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='retencion3'
+           and atg.name='retencion3'
        ) as retencion3
 from account_invoice ai
-	inner join res_partner rp on ai.partner_id=rp.id
-where ai.type='in_invoice'
-	and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
-	and ai.state in ('open','paid')
-	and ((ai.sv_importacion_number is null) or (trim(ai.sv_importacion_number)=''))
+    inner join res_partner rp on ai.partner_id=rp.id
+where ai.company_id= {0}
+    and date_part('year',COALESCE(ai.sv_fecha_tax,ai.date_invoice))=  {1}
+    and date_part('month',COALESCE(ai.sv_fecha_tax,ai.date_invoice))=  {2}
+    and ai.type='in_invoice'
+    and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
+    and ai.state in ('open','paid')
+    and ((ai.sv_importacion_number is null) or (trim(ai.sv_importacion_number)=''))
 
 union all
 
-select 20000000+SI.id as id
-			   ,SI.company_id as company_id
-               ,SI.mes as mes
-               ,SI.anio as anio
-			   ,min(SI.fecha) as fecha
+select min(SI.fecha) as fecha
                ,SI.sv_importacion_number as factura
                ,'DGT' as proveedor
                ,'DGT' as NRC
@@ -127,96 +122,89 @@ select 20000000+SI.id as id
                ,sum (SI.Percibido) as  Percibido
                ,sum (SI.Excluido) as  Excluido
                ,sum (SI.retencion3) as  retencion3
-from (
-select ai.id as id
-    ,ai.company_id as company_id
-    ,date_part('year',COALESCE(ai.sv_fecha_tax,ai.date_invoice)) as anio
-    ,date_part('month',COALESCE(ai.sv_fecha_tax,ai.date_invoice)) as mes
-	,ai.date_invoice as fecha
-	,ai.reference as factura
-	,ai.sv_importacion_number
-	,rp.name as proveedor
-	,rp.vat as NRC
-	,case
-		when rp.country_id=211 then False
-		when rp.country_id is null then False
-		else True end as Importacion
-	,/*Calculando el gravado (todo lo que tiene un impuesto aplicado de iva)*/
+from (select ai.date_invoice as fecha
+    ,ai.reference as factura
+    ,ai.sv_importacion_number
+    ,rp.name as proveedor
+    ,rp.vat as NRC
+    ,case
+        when rp.country_id=211 then False
+        when rp.country_id is null then False
+        else True end as Importacion
+    ,/*Calculando el gravado (todo lo que tiene un impuesto aplicado de iva)*/
      (select coalesce(sum(price_subtotal_signed),0.00)
       from account_invoice_line ail
       where invoice_id=ai.id
-	      and exists(select ailt.tax_id
-					from account_invoice_line_tax ailt
-				        inner join account_tax atx on ailt.tax_id=atx.id
-				        inner join account_tax_group atg on atx.tax_group_id=atg.id
-			         where ailt.invoice_line_id=ail.id and atg.name='iva')
+          and exists(select ailt.tax_id
+                    from account_invoice_line_tax ailt
+                        inner join account_tax atx on ailt.tax_id=atx.id
+                        inner join account_tax_group atg on atx.tax_group_id=atg.id
+                     where ailt.invoice_line_id=ail.id and atg.name='iva')
       ) as Gravado,
       /*Calculando el excento que no tiene iva*/
      (Select coalesce(sum(price_subtotal_signed),0.00)
       from account_invoice_line ail
       where invoice_id=ai.id
-	      and not exists(select ailt.tax_id
-						 from account_invoice_line_tax ailt
-				             inner join account_tax atx on ailt.tax_id=atx.id
-				             inner join account_tax_group atg on atx.tax_group_id=atg.id
-			             where ailt.invoice_line_id=ail.id and atg.name='iva')
+          and not exists(select ailt.tax_id
+                         from account_invoice_line_tax ailt
+                             inner join account_tax atx on ailt.tax_id=atx.id
+                             inner join account_tax_group atg on atx.tax_group_id=atg.id
+                         where ailt.invoice_line_id=ail.id and atg.name='iva')
       ) as Exento
       ,/*Calculando el iva*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='iva'
+           and atg.name='iva'
        ) as Iva
-	   ,/*Calculando el retenido*/
+       ,/*Calculando el retenido*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='retencion'
+           and atg.name='retencion'
        ) as Retenido
-	    ,/*Calculando el percibido*/
+        ,/*Calculando el percibido*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='percepcion'
+           and atg.name='percepcion'
        ) as Percibido
          ,/*Calculando el excluido*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='excluido'
+           and atg.name='excluido'
        ) as Excluido
-	   ,/*Calculando el retencion a terceros*/
+       ,/*Calculando el retencion a terceros*/
       (Select coalesce(sum(ait.amount),0.00)
        from account_invoice_tax ait
- 	       inner join account_tax atx on ait.tax_id=atx.id
-	       inner join account_tax_group atg on atx.tax_group_id=atg.id
+            inner join account_tax atx on ait.tax_id=atx.id
+           inner join account_tax_group atg on atx.tax_group_id=atg.id
        where invoice_id=ai.id
-	       and atg.name='retencion3'
+           and atg.name='retencion3'
        ) as retencion3
 from account_invoice ai
-	inner join res_partner rp on ai.partner_id=rp.id
-where ai.type='in_invoice'
-	and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
-	and ai.state in ('open','paid')
-	and ((ai.sv_importacion_number is not null) or (trim(ai.sv_importacion_number)!=''))
-) SI
-group by   SI.sv_importacion_number, SI.id, SI.anio, SI.mes, SI.company_id
+    inner join res_partner rp on ai.partner_id=rp.id
+where ai.company_id= {0}
+    and date_part('year',COALESCE(ai.sv_fecha_tax,ai.date_invoice))= {1}
+    and date_part('month',COALESCE(ai.sv_fecha_tax,ai.date_invoice))= {2}
+    and ai.type='in_invoice'
+    and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
+    and ai.state in ('open','paid')
+    and ((ai.sv_importacion_number is not null) or (trim(ai.sv_importacion_number)!=''))
+) SI group by   SI.sv_importacion_number
 
 union all
 
-select 30000000+aml.id as id
-               ,am.company_id as company_id
-               ,date_part('year', am.date) as anio
-               ,date_part('month', am.date) as mes
-			   ,aml.date as fecha
+select aml.date as fecha
                ,am.ref as factura
                ,rp.name as proveedor
                ,rp.vat as NRC
@@ -229,12 +217,17 @@ select 30000000+aml.id as id
                ,0.0  as  Excluido
                ,0.0 as  retencion3
  from account_move_line aml
- 	inner join account_move am on aml.move_id=am.id
- 	inner join account_tax at on aml.account_id=at.account_id
- 	inner join account_tax_group atg on at.tax_group_id=atg.id
- 	left join res_partner rp on aml.partner_id=rp.id
- where atg.name='percepcion' and not exists (select id from account_invoice ai where ai.move_id=am.id)
-	and am.state='posted') S order by s.Fecha, s.Factura)""")
+     inner join account_move am on aml.move_id=am.id
+     inner join account_tax at on aml.account_id=at.account_id
+     inner join account_tax_group atg on at.tax_group_id=atg.id
+     left join res_partner rp on aml.partner_id=rp.id
+ where atg.name='percepcion' and not exists (select id from account_invoice ai where ai.move_id=am.id and ai.company_id= {0} )
+     and date_part('year',am.date)= {1}
+    and date_part('month',am.date)= {2}
+    and am.company_id= {0}
+    and am.state='posted') S order by s.Fecha, s.Factura)""".format(company_id,date_year,date_month)
+    self.env.cr.execute(sql)
+    return self.env.cr.fetchall()
 
 class sv_taxpayer_sales_report(models.Model):
     _name = 'strategiksv.reportesv.sales_taxpayer.report'
@@ -382,4 +375,5 @@ class sv_consumer_sales_reportWizard(models.TransientModel):
 
     def _print_report(self, data):
         data['form'].update(self.read(['company_id','date_month','date_year','serie_lenght', 'show_serie'])[0])
+        reporte = sv_purchase_report.create_view(data['form'][0],data['form'][2],data['form'][1])
         #return self.env['strategiksv.reportesv.sales_consumer.report']
