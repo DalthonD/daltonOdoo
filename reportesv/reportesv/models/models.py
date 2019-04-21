8b5031245679487f65c4b6a87279aee5e7b56639
Dalton Diaz <carlos.diaz@strategi-k.com>
2019-04-21 05:17:28 -0600

    q

--- reportesv/reportesv/models/models.py
+++ reportesv/reportesv/models/models.py
@@ -11,9 +11,9 @@ class sv_purchase_report(models.AbstractModel):
     _description = "Reporte de Compras"
     _auto = False
 
-    #@staticmethod
-    def __init__(self,sql):
-        s = sql
+    @staticmethod
+    def init(*sql):
+        s = sql[0]
         #return create_view(s)
 
     @api.model_cr
@@ -232,7 +232,7 @@ class sv_reportWizard(models.TransientModel):
         and date_part('month',am.date)= {2}
         and am.company_id= {0}
         and am.state='posted') S order by s.Fecha, s.Factura)""".format(data['form'][0]['company_id'][0],data['form'][0]['date_year'],data['form'][0]['date_month'])
-            reporte = sv_purchase_report(self._sql)
+            #reporte = sv_purchase_report(self._sql)
         else:
             raise NameError(data['form'],data['form'][0]['company_id'][0],data['form'][0]['date_year'],data['form'][0]['date_month'])
 
