0b96bbddeb6b994afd307ffbf4211daa67b99af8
DalthonD <49283389+DalthonD@users.noreply.github.com>
2019-05-09 16:16:17 -0600

    footer añadido

--- reportesv/reportesv/models/res_company.py
+++ reportesv/reportesv/models/res_company.py
@@ -221,6 +221,101 @@ class res_company(models.Model):
         return data
 
     @api.multi
+    def get_taxpayer_details(self, company_id, date_year, date_month):
+        #data = {}
+        data = []
+        sql = """CREATE OR REPLACE VIEW strategiksv_reportesv_taxpayer_report AS (select * from(
+        select ai.date_invoice as fecha
+        ,ai.reference as factura
+        ,rp.name as cliente
+        ,rp.vat as NRC
+        ,ai.state as estado
+        ,/*Calculando el gravado (todo lo que tiene un impuesto aplicado de iva)*/
+        (select coalesce(sum(price_subtotal_signed),0.00)
+        from account_invoice_line ail
+        where invoice_id=ai.id
+        and exists(select ailt.tax_id
+        from account_invoice_line_tax ailt
+        inner join account_tax atx on ailt.tax_id=atx.id
+        inner join account_tax_group atg on atx.tax_group_id=atg.id
+        where ailt.invoice_line_id=ail.id and atg.name='iva')
+        ) as Gravado,
+        /*Calculando el excento que no tiene iva*/
+        (Select coalesce(sum(price_subtotal_signed),0.00)
+        from account_invoice_line ail
+        where invoice_id=ai.id
+        and not exists(select ailt.tax_id
+        from account_invoice_line_tax ailt
+        inner join account_tax atx on ailt.tax_id=atx.id
+        inner join account_tax_group atg on atx.tax_group_id=atg.id
+        where ailt.invoice_line_id=ail.id and atg.name='iva')
+        ) as Exento
+        ,/*Calculando el iva*/
+        (Select coalesce(sum(ait.amount),0.00)
+        from account_invoice_tax ait
+        inner join account_tax atx on ait.tax_id=atx.id
+        inner join account_tax_group atg on atx.tax_group_id=atg.id
+        where invoice_id=ai.id
+        and atg.name='iva'
+        )*(case when ai.type='out_refund' then -1 else 1 end) as Iva
+        ,/*Calculando el retenido*/
+        (Select coalesce(sum(ait.amount),0.00)
+        from account_invoice_tax ait
+        inner join account_tax atx on ait.tax_id=atx.id
+        inner join account_tax_group atg on atx.tax_group_id=atg.id
+        where invoice_id=ai.id
+        and atg.name='retencion'
+        ) as Retenido
+        ,/*Calculando el percibido*/
+        (Select coalesce(sum(ait.amount),0.00)
+        from account_invoice_tax ait
+        inner join account_tax atx on ait.tax_id=atx.id
+        inner join account_tax_group atg on atx.tax_group_id=atg.id
+        where invoice_id=ai.id
+        and atg.name='percepcion'
+        ) as Percibido
+        from account_invoice ai
+        inner join res_partner rp on ai.partner_id=rp.id
+        left join account_fiscal_position afp on ai.fiscal_position_id=afp.id
+        where ai.company_id= {0}
+        and date_part('year',COALESCE(ai.sv_fecha_tax,ai.date_invoice))= {1}
+        and date_part('month',COALESCE(ai.sv_fecha_tax,ai.date_invoice))= {2}
+        and ((ai.type='out_invoice') or (ai.type='out_refund'))
+        and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
+        and afp.sv_contribuyente=True
+        and ai.state in ('open','paid')
+
+        union
+
+        select ai.date_invoice as fecha
+        ,ai.reference as factura
+        ,'Anulado' as cliente
+        ,rp.vat as NRC
+        ,ai.state as estado
+        ,0.0 as Gravado
+        ,0.0 as Exento
+        ,0.0 as Iva
+        ,0.0 as Retenido
+        ,0.0 as Percibido
+        from account_invoice ai
+        inner join res_partner rp on ai.partner_id=rp.id
+        left join account_fiscal_position afp on ai.fiscal_position_id=afp.id
+        where ai.company_id=  {0}
+        and date_part('year',COALESCE(ai.sv_fecha_tax,ai.date_invoice))=  {1}
+        and date_part('month',COALESCE(ai.sv_fecha_tax,ai.date_invoice))= {2}
+        and ai.type='out_invoice'
+        and ((ai.sv_no_tax is null ) or (ai.sv_no_tax=false))
+        and afp.sv_contribuyente=True
+        and ai.state in ('cancel')
+        )S order by s.fecha, s.factura)""".format(company_id,date_year,date_month)
+        tools.drop_view_if_exists(self._cr, 'strategiksv_reportesv_taxpayer_report')
+        self._cr.execute(sql)
+        if self._cr.description: #Verify whether or not the query generated any tuple before fetching in order to avoid PogrammingError: No results when fetching
+            #data = self._cr.dictfetchall()
+            data = list(self._cr.fetchall())
+        return data
+
+    @api.multi
     def get_month_str(self, month):
         m = "No especificado"
         if self and month>0:
