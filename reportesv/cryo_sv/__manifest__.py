d887655ae349844190ffe496f93d6bc08489c47a
DalthonD <49283389+DalthonD@users.noreply.github.com>
2019-05-13 11:57:51 -0600

    Adicion de modulos de PoS
    Adicion de modulos de PoS

---
+++ reportesv/cryo_sv/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "CRYO SV",
+    "category": 'Sales',
+    "summary": """
+       Localizacion de CRYO .""",
+    "description": """
+   Registra la orden de produccions y POS  para CRYO
+    """,
+    "sequence": 1,
+    "author": "Strategi-k",
+    "website": "http://strategi-k.com",
+    "version": '12.0.0.5',
+    "depends": ['sale','base','point_of_sale',],
+    "data": [
+        'security/ir.model.access.csv',
+    'views/assets.xml',
+    'views/pos_config.xml',
+    ],
+    "qweb": ['static/src/xml/pos.xml'],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
