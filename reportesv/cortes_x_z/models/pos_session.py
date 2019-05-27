c959aa6fa55b3070ecde02b9d9a9b2b3beee1cd4
DalthonD <49283389+DalthonD@users.noreply.github.com>
2019-05-27 09:53:18 -0600

    ordernamiento de numero de referencia de factura en caso de desorden

--- reportesv/cortes_x_z/models/pos_session.py
+++ reportesv/cortes_x_z/models/pos_session.py
@@ -697,6 +697,7 @@ class pos_session(models.Model):
                     for invoice in pos_invoice_obj:
                         if order.invoice_id==invoice.id:
                             pos_invoice_obj.append(invoice)
+                pos_invoice_obj.sorted(key=lambda i: i.reference)
                 if len(fiscal_position_ids)>1 and pos_invoice_obj:
                     for inv in pos_invoice_obj:
                         if inv.fiscal_position_id in fiscal_position_ids:
